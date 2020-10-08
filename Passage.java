9c1651dea7d37deb4d8d349446248100711d36fb
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 17:10:55 -0600

    Create Passage.java

---
+++ Passage.java
@@ -0,0 +1,15 @@
+public class Passage extends Structure{
+
+  public Passage(){
+    System.out.println("Passage");
+    }
+    
+   public setName(string s){
+    System.out.println("setName");
+    }
+    
+    public setId(int room1,int room2){
+    System.out.println("setId");
+    }
+    
+ }

d6ed539e2bbd696b0c3ab5a6222bff32b6695307
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 17:08:20 -0600

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,15 @@
+public class Room extends Structure{
+
+  public Room(string){
+    System.out.println("Room");
+    }
+    
+   public setId(int room){
+    System.out.println("setId");
+    }
+    
+    public setCreature(Creature Monster){
+    System.out.println("setCreature");
+    }
+}
+    

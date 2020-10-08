6a023287f27c528c6f82a5891913dd2f9f4bc217
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 16:47:37 -0600

    Create Displayable.java

---
+++ Displayable.java
@@ -0,0 +1,53 @@
+public class Displayable{
+  
+  public Displayable(){
+  System.out.println("Displayable");
+  }
+  
+  public void setInvisible(){
+  System.out.println("setInvisible");
+  }
+  
+  public void setVisible(){
+  System.out.println("setVisible");
+  }
+  
+  public void setMaxHit(int maxHit){
+  System.out.println("setMaxHit");
+  }
+  
+  public void setHpMove(int hpMoves){
+  System.out.println("setHpMove");
+  }
+  
+  public void setHp(int Hp){
+  System.out.println("setHp");
+  }
+  
+  public void setType(char t){
+  System.out.println("setType");
+  }
+  public void setValue(int v){
+  System.out.println("setValue");
+  }
+  
+  public void setPosX(int x){
+  System.out.println("setPosX");
+  }
+  
+  public void setPosY(int y){
+  System.out.println("setPosY");
+  }
+  
+  public void setWidth(int x){
+  System.out.println("setWidth");
+  }
+  
+  public void setHeight(int y){
+  System.out.println("setHeight");
+  }
+  
+}
+  
+  
+  

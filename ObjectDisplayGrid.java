6589f143cdd02611f070b8fa9f562906adc8ed6f
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 16:35:59 -0600

    Create ObjectDisplayGrid.java

---
+++ ObjectDisplayGrid.java
@@ -0,0 +1,12 @@
+public class ObjectDisplayGrid{
+
+  getObjectDisplayGrid(int gameHeight, int width, int topHeight) {
+    System.out.println("getObjectDisplayGrid");
+  }
+  
+   setTopMessageHeight(int topHeight){
+    System.out.println("setTopMessageHeight");
+    }
+    
+}
+   

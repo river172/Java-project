8f2ea08bcb3ff56be0b08e258a560d7ee9125048
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 18:29:31 -0600

    Create Armor.java

---
+++ Armor.java
@@ -0,0 +1,15 @@
+public class Armor extends Item{
+
+  public Armor(string Name){
+    System.out.println("Armor");
+    }
+    
+    public void setName(string name){
+    System.out.println("setName");
+    }
+    
+    public void setID(int room,int serial){
+    System.out.println("setID");
+    }
+    
+ }

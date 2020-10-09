c1008e438c420e1a292e8447de62473bfa0217e5
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 18:22:15 -0600

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player extends creature{
+
+  public void setWeapon(Item sword){
+    System.out.println("setWeapon");
+    }
+    
+    public void setArmor(Item armor){
+    System.out.println("setArmor");
+    }
+    
+ }

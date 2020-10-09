86d377fa5628030af67e3896ef3cfd5a9ea87385
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 18:31:15 -0600

    Create Sword.java

---
+++ Sword.java
@@ -0,0 +1,11 @@
+public class Sword extends Item{
+
+  public Sword(string name){
+    System.out.println("Sword");
+    }
+    
+    public void setID(int room,int serial){
+    System.out.println("setID");
+    }
+    
+    }

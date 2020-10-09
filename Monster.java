8d10ab61d5c4d7da2626d5bde5cd9448298c600e
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 18:24:20 -0600

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,15 @@
+public class Monster extends Creature{
+  
+  public Monster(){
+    System.out.println("Monster");
+    }
+    
+    public void setName(string name){
+    System.out.println("setName");
+    }
+    
+     public void setID(int room,int serial){
+    System.out.println("setID");
+    }
+  
+}

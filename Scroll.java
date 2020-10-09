8520d9fc0fb9f50e6490c1c4c85cb62e5aae8c45
river172 <66964986+river172@users.noreply.github.com>
2020-10-08 18:27:44 -0600

    Create Scroll.java

---
+++ Scroll.java
@@ -0,0 +1,11 @@
+public class Scroll extends Item{
+  public Scroll(string name){
+    System.out.println("Scroll");
+    }
+    
+    public setID(int room,int serial){
+    System.out.println("setID");
+    }
+}
+
+  

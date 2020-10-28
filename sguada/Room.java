d51aed0fe4dcc5438b305189c60a4af39bfcf7c6
river172 <66964986+river172@users.noreply.github.com>
2020-10-28 11:04:35 -0600

    Update Room.java

--- sguada/Room.java
+++ sguada/Room.java
@@ -1,14 +1,24 @@
 public class Room extends Structure{
+  private String roomN;
+  private int ID;
+  private Creature mons;
   public Room(String string){
+    roomN = string;
     System.out.println("Room");
   }
     
   public void setId(int room){
+    ID = room;
     System.out.println("setId");
   }
     
   public void setCreature(Creature Monster){
+    mons = monster;
     System.out.println("setCreature");
+  }
+  
+  public int getID(){
+    return ID;
   }
 }
     

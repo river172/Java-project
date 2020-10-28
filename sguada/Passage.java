9ead2afdbf3a825e03b031bb260823ce7f576369
river172 <66964986+river172@users.noreply.github.com>
2020-10-28 11:00:56 -0600

    Update Passage.java

--- sguada/Passage.java
+++ sguada/Passage.java
@@ -1,15 +1,27 @@
 public class Passage extends Structure{
+  private String name;
+  private int room;
+  private int _room2;
 
   public Passage(){
     System.out.println("Passage");
   }
     
   public void setName(String string){
+    name = string;
     System.out.println("setName");
   }
     
   public void setID(int room1,int room2){
+    room = room1;
+    _room2 = room2;
     System.out.println("setId");
   }
+  
+  public String getName(){
+    
+    return name;
+  }
+ 
     
  }

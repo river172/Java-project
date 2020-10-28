82f8d93583f956ac834a8dcabdc21854f834731b
river172 <66964986+river172@users.noreply.github.com>
2020-10-28 11:09:04 -0600

    Update Player.java

--- sguada/Player.java
+++ sguada/Player.java
@@ -1,10 +1,15 @@
 public class Player extends Creature{
+  
+  private Item _sword;
+  private Item _armor;
 
   public void setWeapon(Item sword){
+    _sword = sword;
     System.out.println("setWeapon");
   }
     
   public void setArmor(Item armor){
+    _armor = armor;
     System.out.println("setArmor");
   }
     

cf10a54e4596b9f20d86c9c247e69a6bf90dcff4
river172 <66964986+river172@users.noreply.github.com>
2020-10-28 10:57:32 -0600

    Update ObjectDisplayGrid.java

--- sguada/ObjectDisplayGrid.java
+++ sguada/ObjectDisplayGrid.java
@@ -1,12 +1,23 @@
 public class ObjectDisplayGrid{
+  
+  private int gameH;
+  private int _width;
+  private int _topheight;
 
   public void getObjectDisplayGrid(int gameHeight, int width, int topHeight) {
+    gameH = gameHeight;
+    _width = width;
+    _topheight = topHeight;
     System.out.println("getObjectDisplayGrid");
   }
   
    public void setTopMessageHeight(int topHeight){
+     _topheight = height
     System.out.println("setTopMessageHeight");
     }
+  public int getTopMessageHeight(){
+    return _topheight;
+  }
     
 }
    
